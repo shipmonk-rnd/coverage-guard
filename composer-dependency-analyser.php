61f542091ec47a44a31d9d382facf686594b2be9
Jan Nedbal <janedbal@gmail.com>
2025-10-06 17:10:24 +0200

    Support all versions of sebastian/diff and make it optional dependency (#8)

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -5,6 +5,7 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
 $config->ignoreErrorsOnPackage('phpunit/php-code-coverage', [ErrorType::DEV_DEPENDENCY_IN_PROD]); // optional dependency to load .cov files
+$config->ignoreErrorsOnPackage('sebastian/diff', [ErrorType::DEV_DEPENDENCY_IN_PROD]); // optional dependency to parse patch files
 $config->ignoreErrorsOnExtension('ext-tokenizer', [ErrorType::SHADOW_DEPENDENCY]); // optional dependency to have syntax highlighting
 $config->ignoreErrorsOnExtensions(['ext-simplexml', 'ext-libxml'], [ErrorType::SHADOW_DEPENDENCY]); // optional dependency to load clover/cobertura files
 
