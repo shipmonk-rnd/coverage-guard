9e96db935d597a5d8d332952055879c9ba7450a7
Jan Nedbal <janedbal@gmail.com>
2025-11-05 12:56:46 +0100

    Indenter: improve, add test

--- src/Utils/Indenter.php
+++ src/Utils/Indenter.php
@@ -3,7 +3,7 @@
 namespace ShipMonk\CoverageGuard\Utils;
 
 use LogicException;
-use function floor;
+use function preg_quote;
 use function preg_replace_callback;
 use function str_repeat;
 use function strlen;
@@ -11,18 +11,23 @@ use function strlen;
 final class Indenter
 {
 
+    /**
+     * @param string $from Original indent expected in $code
+     * @param string $to New indent to be used
+     */
     public static function change(
         string $code,
         string $from,
         string $to,
     ): string
     {
+        $escapedFrom = preg_quote($from, '/');
         $out = preg_replace_callback(
-            pattern: '/^( +)/m',
+            pattern: '/^((?:' . $escapedFrom . ')+)/m',
             callback: static function (array $matches) use ($from, $to): string {
-                $currentIndentLength = strlen($matches[1]);
-                $level = (int) floor($currentIndentLength / strlen($from));
-                return str_repeat($to, $level);
+                $currentIndentLength = strlen($matches[1]); // @phpstan-ignore offsetAccess.notFound
+                $level = $currentIndentLength / strlen($from);
+                return str_repeat($to, (int) $level);
             },
             subject: $code,
         );
