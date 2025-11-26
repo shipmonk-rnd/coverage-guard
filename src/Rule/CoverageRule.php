0d485fa9602711a19eec36d8d6e4cfcc06b5ecdd
Jan Nedbal <janedbal@gmail.com>
2025-11-26 09:33:25 +0100

    Rule phpdocs

--- src/Rule/CoverageRule.php
+++ src/Rule/CoverageRule.php
@@ -5,11 +5,16 @@ namespace ShipMonk\CoverageGuard\Rule;
 use ShipMonk\CoverageGuard\Hierarchy\CodeBlock;
 
 /**
+ * Implementors are expected to be passed to ShipMonk\CoverageGuard\Config::addRule() method
+ *
  * @api
  */
 interface CoverageRule
 {
 
+    /**
+     * @param bool $patchMode True when --patch option is used (and only changed code blocks are analyzed)
+     */
     public function inspect(
         CodeBlock $codeBlock,
         bool $patchMode,
