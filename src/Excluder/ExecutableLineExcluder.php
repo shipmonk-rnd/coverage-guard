d4db696859e141207cad31e8499d8fe14e3cd6ad
Jan Nedbal <janedbal@gmail.com>
2025-12-04 14:21:33 +0100

    Drop NormalizeMultilineCallsLineExcluder - not working properly when Closure is passed as argument.

--- src/Excluder/ExecutableLineExcluder.php
+++ src/Excluder/ExecutableLineExcluder.php
@@ -12,9 +12,7 @@ interface ExecutableLineExcluder
 
     /**
      * Provided lines will be treated as not-executable
-     * This can help you normalize/adjust better coverage percentage by for example:
-     *    - Counting method calls as single line no matter if written as multiline or not
-     *    - Excluding lines you don't want to cover (e.g. throw new LogicException() calls)
+     *  e.g. to exclude lines you don't want to cover (like throw new LogicException() calls)
      */
     public function getExcludedLineRange(Node $node): ?ExcludedLineRange;
 
