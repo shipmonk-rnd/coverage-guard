8912d9ed1402cfa32b7613f766151fb66ad95ee9
Jan Nedbal <janedbal@gmail.com>
2025-12-04 14:29:34 +0100

    IgnoreThrowNewExceptionLineExcluder: support old parser v4

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,6 +4,7 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 $config = new Configuration();
+$config->ignoreUnknownClasses(['PhpParser\Node\Stmt\Throw_']); // old version of php-parser
 $config->ignoreErrorsOnPackage('phpunit/php-code-coverage', [ErrorType::DEV_DEPENDENCY_IN_PROD]); // optional dependency to load .cov files
 $config->ignoreErrorsOnPackage('sebastian/diff', [ErrorType::DEV_DEPENDENCY_IN_PROD]); // optional dependency to parse patch files
 $config->ignoreErrorsOnExtension('ext-tokenizer', [ErrorType::SHADOW_DEPENDENCY]); // optional dependency to have syntax highlighting
--- src/Excluder/IgnoreThrowNewExceptionLineExcluder.php
+++ src/Excluder/IgnoreThrowNewExceptionLineExcluder.php
@@ -6,11 +6,11 @@ use PhpParser\Node;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\Throw_;
 use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\Throw_ as OldThrow_;
 use function in_array;
 
 /**
  * Excludes lines for "throw new YourException()" statements
- * - only works with PHPParser v5
  *
  * @api
  */
@@ -29,7 +29,7 @@ final class IgnoreThrowNewExceptionLineExcluder implements ExecutableLineExclude
     public function getExcludedLineRange(Node $node): ?ExcludedLineRange
     {
         if (
-            $node instanceof Throw_
+            ($node instanceof Throw_ || $node instanceof OldThrow_)
             && $node->expr instanceof New_
             && $node->expr->class instanceof Name
             && in_array($node->expr->class->toString(), $this->classNames, true)
